3d0c4a9f4346def450d6bbb13da295e13921ba80
skidroww <lovehor99@naver.com>
2026-05-10 22:27:13 +0900

    회원가입 성공실패시 팝업창

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from accounts.models import UserProfile
+from django.contrib import messages
 
 def signup(request):
     if request.method == 'POST':
@@ -11,8 +12,11 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             UserProfile.objects.create(user=user, role=user_role)
-            return redirect('/accounts/login/')
+            messages.success(request, '회원가입이 완료되었습니다!')
 
+            return redirect('/accounts/login/')
+        else:
+            messages.error(request, '회원가입에 실패했습니다. 입력 내용을 확인해주세요.')
     else:
         form = UserCreationForm()
 
