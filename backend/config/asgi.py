bf44a247bc554a1a32d1a7e6243d1e870e05fc3a
skidroww <lovehor99@naver.com>
2026-06-09 19:52:28 +0900

    uvicorn config.asgi:application --host

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
+import sys
+from pathlib import Path
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_asgi_application()
+
+root_path = Path(__file__).resolve().parent.parent.parent  # SKN25-FINAL-3Team 루트
+sys.path.append(str(root_path))
